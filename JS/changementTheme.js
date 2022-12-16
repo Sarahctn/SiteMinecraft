17c3d926f350c992c106458d1c9adb49b930b130
Sarah Ripoche <sarah.ripoche@etu.univ-amu.fr>
2022-12-16 21:28:07 +0100

    fix javascript

--- JS/changementTheme.js
+++ JS/changementTheme.js
@@ -1,8 +1,8 @@
 function toggleTheme() {
     var theme = document.getElementsByTagName('link')[0];
-    if (theme.getAttribute('href') == '/css/pages_sauf_index.css') {
-        theme.setAttribute('href', '/css/pages_sauf_index_sombre.css');
+    if (theme.getAttribute('href') == '/SiteMinecraft/css/pages_sauf_index.css') {
+        theme.setAttribute('href', '/SiteMinecraft/css/pages_sauf_index_sombre.css');
     } else {
-        theme.setAttribute('href', '/css/pages_sauf_index.css');
+        theme.setAttribute('href', '/SiteMinecraftS/css/pages_sauf_index.css');
     }
 }
