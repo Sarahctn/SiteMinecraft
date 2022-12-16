eec86799838d9b1c585dab98eef9c6e158ace641
Sarah <sarah.ripoche@etu.univ-amu.fr>
2022-12-16 11:42:08 +0100

    Ajout js

---
+++ JS/changementTheme.js
@@ -0,0 +1,8 @@
+function toggleTheme() {
+    var theme = document.getElementsByTagName('link')[0];
+    if (theme.getAttribute('href') == '../css/pages_sauf_index.css') {
+        theme.setAttribute('href', '../css/pages_sauf_index_sombre.css');
+    } else {
+        theme.setAttribute('href', '../css/pages_sauf_index.css');
+    }
+}
